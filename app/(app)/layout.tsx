2e4ce8a8d74874f3ab4ca247272d14e02047e9c2
hhhiteshhh <hiteshkumar1425@gmail.com>
2025-12-22 09:59:03 +0530

    shadcn sanity clerk added

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,13 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { SanityLive } from "@/sanity/lib/live";
+
+function AppLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider>
+      <main>{children}</main>
+      <SanityLive />
+    </ClerkProvider>
+  );
+}
+
+export default AppLayout;
